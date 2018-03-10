a2a2298d7aabff93fc4b6ea228f0ba2fc44c3969
Mizuki Zenta <mizukisb@gmail.com>
2018-03-10 13:06:36 +1100

    init with bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./App.css"
+
+// add bootstrap
+import "bootstrap/dist/css/bootstrap.css"
+
+class App extends Component {
+  render() {
+    return <div className="App text-center">Hello world</div>
+  }
+}
+
+export default App
