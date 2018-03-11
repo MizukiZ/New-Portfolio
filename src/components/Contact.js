524292da9d8dcdeb49de97ea4c05007eb1ebf551
Mizuki Zenta <mizukisb@gmail.com>
2018-03-11 16:35:40 +1100

    adjust vh and header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Contact({ position }) {
   return (
-    <div className="contact-wrap text-center m-2">
+    <div className="contact-wrap text-center ">
       <div className="contact-subWrap">
         <div className="contact-title mb-3">
           <span>C</span>ontact
